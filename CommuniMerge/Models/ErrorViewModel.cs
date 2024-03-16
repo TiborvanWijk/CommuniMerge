6571ea411a6242425408409e3422a6f06fe662f7
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2024-03-16 18:06:55 +0100

    Setup

---
+++ CommuniMerge/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CommuniMerge.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
