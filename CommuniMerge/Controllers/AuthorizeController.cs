59e02f7c3e74f1e3d16692ea89c310f3dd2403f9
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2024-03-21 19:18:56 +0100

    start of register/login page

---
+++ CommuniMerge/Controllers/AuthorizeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommuniMerge.Controllers
+{
+    public class AuthorizeController : Controller
+    {
+        public AuthorizeController()
+        {
+            
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
