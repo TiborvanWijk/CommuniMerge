078c26f9b40a3b78c3b1825198d3ec6acaf98142
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2024-03-24 16:24:42 +0100

    csproj files

---
+++ CommuniMerge.Library/Repositories/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommuniMerge.Library.Repositories
+{
+    internal class UserRepository
+    {
+    }
+}
