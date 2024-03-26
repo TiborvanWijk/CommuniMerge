057f242cc84ed917db46b63e98565c233a0953cf
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2024-03-26 15:21:01 +0100

    login/register setup

---
+++ CommuniMerge.Library/Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommuniMerge.Library.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ CommuniMerge/Controllers/AuthorizeController.cs
@@ -0,0 +1,35 @@
+﻿using CommuniMerge.Library.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommuniMerge.Controllers
+{
+    public class AuthorizeController : Controller
+    {
+        private readonly HttpClient client;
+
+        public AuthorizeController()
+        {
+            this.client = new HttpClient();
+            this.client.BaseAddress = new Uri("https://localhost:7129");
+        }
+        public IActionResult Login()
+        {
+            return View(new LoginModel() { Username = "", Password = ""});
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            return View(model);
+        }
+
+        public async Task<IActionResult> Register()
+        {
+            return View(new RegisterModel() { Email = "", Password = "", Username = "" });
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            return View(model);
+        }
+    }
+}
